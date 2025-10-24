ef03d61645c8f538f2f8d56c65a5535628f11c35
Ludmila Valeeva <valeevaludmila748@gmail.com>
2025-10-24 18:22:47 +0300

    сохраняю изменения

---
+++ stellar-burgers/conftest.py
@@ -0,0 +1,65 @@
+import pytest
+import requests
+from urls import Url
+from generators import email_generator, password_generator, name_generator
+from data import TestData
+
+
+@pytest.fixture
+def create_user():
+    """Фикстура для создания пользователя с автоматическим удалением"""
+    email = email_generator()
+    password = password_generator()
+    name = name_generator()
+    
+    user_data = {
+        "email": email,
+        "password": password,
+        "name": name
+    }
+    
+    # Создаем пользователя
+    response = requests.post(f'{Url.MAIN_URL}{Url.CREATE_USER}', json=user_data)
+    
+    token = response.json().get("accessToken")
+    
+    yield user_data, token
+    
+    # Удаляем пользователя после теста
+    if token:
+        requests.delete(
+            f'{Url.MAIN_URL}{Url.USER_PROFILE}',
+            headers={"Authorization": token}
+        )
+
+
+@pytest.fixture
+def generate_user_data():
+    """Фикстура для генерации данных пользователя без создания"""
+    return {
+        "email": email_generator(),
+        "password": password_generator(), 
+        "name": name_generator()
+    }
+
+
+@pytest.fixture
+def setup_existing_user():
+    """Фикстура для подготовки существующего пользователя"""
+    user_data = {
+        "email": TestData.EXISTING_USER_EMAIL,
+        "password": TestData.EXISTING_USER_PASSWORD,
+        "name": TestData.EXISTING_USER_NAME
+    }
+    
+    try:
+        response = requests.post(f'{Url.MAIN_URL}{Url.CREATE_USER}', json=user_data)
+        if response.status_code == 403:
+            login_response = requests.post(f'{Url.MAIN_URL}{Url.LOGIN_USER}', json={
+                "email": user_data["email"],
+                "password": user_data["password"]
+            })
+    except:
+        pass
+    
+    yield user_data
